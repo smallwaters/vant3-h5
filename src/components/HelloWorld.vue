343d8188c80dba87c2a16730de80241c151b0d3c
ZhaiYu <zhaiyu_hb@hotmail.com>
2022-09-18 13:15:56 +0800

    feat: 加入less

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,9 +2,9 @@
 defineProps({
   msg: {
     type: String,
-    required: true
-  }
-})
+    required: true,
+  },
+});
 </script>
 
 <template>
@@ -18,7 +18,7 @@ defineProps({
   </div>
 </template>
 
-<style scoped>
+<style lang="less" scoped>
 h1 {
   font-weight: 500;
   font-size: 2.6rem;
@@ -29,11 +29,12 @@ h3 {
   font-size: 1.2rem;
 }
 
-.greetings h1,
-.greetings h3 {
-  text-align: center;
+.greetings {
+  h1,
+  h3 {
+    text-align: center;
+  }
 }
-
 @media (min-width: 1024px) {
   .greetings h1,
   .greetings h3 {
